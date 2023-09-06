5594eb89edd531a9d2da75a46824ce8f45c9c0f8
Rohan M <motukuri.rohan@gmail.com>
2023-09-06 19:03:19 +0530

    Setup the front end 06-09

---
+++ Sources/Implementation - Standard Stack/Front End/common-mail-spa/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {useState} from 'react';
+
+
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
